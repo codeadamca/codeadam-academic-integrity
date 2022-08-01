a5c6a79d513f1374195d325c79c1608f36d7c252
Adam Thomas <thomasadam83@hotmail.com>
2022-07-31 23:29:45 -0400

    Updated core

--- src/home/HomeHeader.js
+++ src/home/HomeHeader.js
@@ -9,19 +9,24 @@ class HomeHeader extends Component {
 
         <header className="ca-border-bottom-gray">
 
-          <div className="ca-fullscreen">
+          <div className="ca-fullscreen w3-black">
             <video preload="auto" autoPlay loop muted className="ca-fullscreen-background">
                 <source src={homeVideo} />
             </video>
             <div className="ca-fullscreen-content w3-cell-row">
                 <div className="w3-cell w3-cell-middle w3-container w3-center">
                     
-                <h1 className="w3-text-white ca-font-medium ca-pt-sans">Academic Integrity</h1>
-                <h2 className="w3-text-red ca-font-large ca-pt-sans">Coding and Plagiarism</h2>
+                <h1 className="w3-text-white ca-font-medium ca-pt-sans"><span>Adam Thomas</span></h1>
+                <h2 className="w3-text-red ca-font-large ca-pt-sans"><span>I Teach Code!</span></h2>
                 <p className="w3-text-white ca-font-small ca-pt-sans w3-center">
-                    An online course focussing on 
-                    <br />
-                    academic integrity in computer science.
+                  <span>
+                    Self-taught full-stack developer.
+                  </span>
+                </p>
+                <p className="w3-text-white ca-font-small ca-pt-sans w3-center">
+                  <span>
+                    Learning code and teaching code at Humber College, Toronto, Canada.
+                  </span>
                 </p>
                 </div>
             </div>
