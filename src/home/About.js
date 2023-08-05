bbdfccd3aa3de8c71551ac30dac3a5fdd3242dd5
Adam Thomas <thomasadam83@hotmail.com>
2023-08-05 10:22:32 -0400

    Fixed typos on home page

--- src/home/About.js
+++ src/home/About.js
@@ -10,23 +10,19 @@ class About extends Component {
           <h2 className="w3-text-red w3-center">About this Course</h2>
 
           <h3>
-            Writing code for academic purposes is similar to all academic writing in that 
-            students are required to follow similar guidelines. 
+            Writing code for academic purposes is similar to all academic writing in that students are required to follow similar guidelines.
           </h3>
 
           <p>
-            However, identifying what is and what isn't academic misconduct when writing code,
-            assisting fellow students, searching for code samples online, or utilizing 
-            online code samples can be difficult. 
-            This is because academic guidelines related to writing code will vary 
-            in each course and occaisionally in each assignment. 
-            Additionally, the expectations of a programmer during academia are dramatically 
-            different than once a student enters the industry.
+            However, identifying what is and what isn't academic misconduct when writing code, assisting fellow students, searching for code 
+            samples online, or utilizing online code samples can be difficult. This is because academic guidelines related to writing code will 
+            vary in each course and occasionally in each assignment. Additionally, the expectations of a programmer during academia are 
+            dramatically different than the industry.
           </p>
 
           <p>
-             This course will outline those guidelines and require students to complete a 
-             series of quizzes to ensure the guidelines are understood.             
+             This course will outline those guidelines and require students to complete a series of quizzes to ensure the guidelines are 
+             understood.
           </p>
 
         </article>
